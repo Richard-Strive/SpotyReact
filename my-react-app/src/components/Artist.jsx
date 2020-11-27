2bbd38d628c1a46d21adb9f58e487232d2f61f85
Richard-Strive <72205808+Richard-Strive@users.noreply.github.com>
2020-11-27 10:17:37 +0100

    setUp

---
+++ my-react-app/src/components/Artist.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class artist extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default artist;
---
+++ my-react-app/src/components/FooTer.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class FooTer extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+export default FooTer;
